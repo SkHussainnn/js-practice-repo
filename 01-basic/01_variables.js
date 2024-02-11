7c30d10f5d5469191f1f84d4d99d265a31d61efc
Hussain <skiftekhar92@gmail.com>
2024-02-11 07:31:42 +0530

    first commit

---
+++ 01-basic/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345
+let accountEmail = "skiftekhar92@gmail.com"
+var accountPassword = "123456"
+accountCity = "Cuttack"
+let accountState;
+
+//accountId = 123//
+
+
+accountEmail = "hskahzar7861@gmail.com"
+accountPassword = "121212"
+accountCity = "Ranchi"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+
+console. table([accountId, accountEmail, accountPassword, accountCity, accountState])
